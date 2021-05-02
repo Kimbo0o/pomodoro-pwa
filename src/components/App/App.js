89644633ef6a5abdd269ab45ec44b63acef65130
Kimbo0o <kimkoch1000@gmail.com>
2021-05-02 21:52:04 +0200

    Addded timer functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BtnStartStop from '../BtnStartStop/BtnStartStop';
 import BtnTimerSelect from '../BtnTimeSelect/BtnTimerSelect';
 import Timer from '../Timer/Timer';
@@ -6,15 +6,85 @@ import Timer from '../Timer/Timer';
 import './App.scss';
 
 function App() {
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [timerMinutes, setTimerMinutes] = useState("25");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+  const [selectedTimer, setSelectedTimer] = useState(0);
+  let interval = false;
+
   function clickedWorkBtn(evt) {
+    setSelectedTimer(0);
+    setTimerMinutes("25");
+    setTimerSeconds("00");
+    stopTimer();
     console.log("work");
   }
+
   function clickedShortBreakBtn(evt) {
+    setSelectedTimer(1);
+    setTimerMinutes("05");
+    setTimerSeconds("00");
+    stopTimer();
     console.log("short break");
   }
+
   function clickedLongBreakBtn(evt) {
+    setSelectedTimer(2);
+    setTimerMinutes("15");
+    setTimerSeconds("00");
+    stopTimer();
     console.log("long break");
   }
+
+  function clickedStarStopBtn(evt) {
+    if (timerRunning) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
+  }
+
+  function startTimer() {
+    setTimerRunning(true);
+  }
+
+  function stopTimer() {
+    setTimerRunning(false);
+  }
+
+
+  function dateNumberToString(number) {
+    let val = number.toString();
+    if (val.length < 2) {
+      val = "0" + val;
+    }
+    return val;
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      //check if timer is running
+      if (timerRunning) {
+        //parse timer numbers      
+        let minutesNum = parseInt(timerMinutes);
+        let secondsNum = parseInt(timerSeconds);
+        //countdown logic
+        if (secondsNum === 0) {
+          if (minutesNum > 0) {
+            minutesNum--;
+            secondsNum = 59;
+          }
+        } else {
+          secondsNum--;
+        }
+        //convert new counter to strings
+        setTimerMinutes(dateNumberToString(minutesNum));
+        setTimerSeconds(dateNumberToString(secondsNum));
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  })
+
   return (
     <>
       <div className="main-container">
@@ -25,13 +95,13 @@ function App() {
         <div className="glass-container">
           <div className="glass-container-content">
             <div className="timer-select-btns">
-              <BtnTimerSelect title="Work" selected="true" clicked={clickedWorkBtn}></BtnTimerSelect>
-              <BtnTimerSelect title="Short Break" clicked={clickedShortBreakBtn}></BtnTimerSelect>
-              <BtnTimerSelect title="Long Break" clicked={clickedLongBreakBtn}></BtnTimerSelect>
+              <BtnTimerSelect title="Work" selected={selectedTimer === 0} clicked={clickedWorkBtn}></BtnTimerSelect>
+              <BtnTimerSelect title="Short Break" selected={selectedTimer === 1} clicked={clickedShortBreakBtn}></BtnTimerSelect>
+              <BtnTimerSelect title="Long Break" selected={selectedTimer === 2} clicked={clickedLongBreakBtn}></BtnTimerSelect>
             </div>
-            <Timer minutes="24" seconds="59"></Timer>
+            <Timer minutes={timerMinutes} seconds={timerSeconds}></Timer>
             <div className="start-stop-wrap">
-              <BtnStartStop title="START"></BtnStartStop>
+              <BtnStartStop title={timerRunning ? "STOP" : "START"} clicked={clickedStarStopBtn}></BtnStartStop>
             </div>
           </div>
         </div>
